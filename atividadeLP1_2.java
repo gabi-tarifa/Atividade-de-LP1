e27951727fa8492710943f00ae6c54aaf391f86b
gabi-tarifa <126773354+gabi-tarifa@users.noreply.github.com>
2023-03-23 09:11:01 -0300

    Add files via upload

---
+++ atividadeLP1_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class atividadeLP1_2 {
+    public static String nomes[] = {" ", " ", " "};
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+    public static void vetor() {
+    }
+    public static void matriz(){
+        double contas[][] = {{0, 0, 0, 0}, {0, 0, 0, 0} ,{0, 0, 0, 0}, {0, 0, 0, 0}};
+    }
+    public static int retorno(){
+
+    }
+}
